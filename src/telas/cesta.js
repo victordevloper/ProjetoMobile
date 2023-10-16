c0ad8865ffcb899bd0facfafd463bce9abed6261
Victor <luanvitorlima2017@gmail.com>
2023-10-16 07:05:54 -0300

    view de informações criada

--- src/telas/cesta.js
+++ src/telas/cesta.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Image, Dimensions, StyleSheet, Text } from "react-native";
+import { Image, Dimensions, StyleSheet, Text, View } from "react-native";
 import topo from '../../assets/topo.png';
+import logo from '../../assets/logo.png';
 
 const width = Dimensions.get('screen').width;
 
@@ -8,6 +9,16 @@ export default function Cesta(){
     return <>
         <Image style={estilos.topo} source={topo}/>
         <Text style={estilos.titulo}>Detalhes da Cesta</Text>
+        
+        <View style={estilos.cesta}>
+            <Text style={estilos.nome}>Cesta De Verduras</Text>
+            <View style={estilos.fazenda}>
+                <Image style={estilos.imagemFazenda} source={logo}></Image>
+                <Text style={estilos.nomeFazenda}>Fazenda Jack Farm</Text>
+            </View>
+            <Text style={estilos.descricao}> uma cesta com produtos selecionados cuidadosamente da Fazenda diretamente para a cozinha</Text>
+            <Text style={estilos.preco}>R$ 40,00</Text>
+        </View>
     </>
 }
 
@@ -25,5 +36,41 @@ const estilos = StyleSheet.create({
         color: "white",
         fontWeight: "bold",
         padding: 16
+    },
+    nome:{
+        fontSize: 26,
+        fontWeight: "bold",
+        lineHeight: 42,
+        color: "#464646",
+    },
+    nomeFazenda:{
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12
+    },
+    cesta:{
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    descricao:{
+        color: "#a3a3a3",
+        fontSize: 16,
+        lineHeight: 26,
+    },
+    preco:{
+        color: "#2a9f85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
+    },
+    fazenda:{
+        flexDirection: "row",
+        paddingVertical: 12
+    },
+    imagemFazenda:{
+        width: 32,
+        height: 32,
     }
+    
 })
