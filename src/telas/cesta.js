3f076a75326ff7e1962a26ecec2cf61ce2e760ae
Victor <luanvitorlima2017@gmail.com>
2023-10-16 06:46:21 -0300

    capa pronta

--- src/telas/cesta.js
+++ src/telas/cesta.js
@@ -7,7 +7,7 @@ const width = Dimensions.get('screen').width;
 export default function Cesta(){
     return <>
         <Image style={estilos.topo} source={topo}/>
-        <Text>Detalhes da Cesta</Text>
+        <Text style={estilos.titulo}>Detalhes da Cesta</Text>
     </>
 }
 
@@ -15,5 +15,15 @@ const estilos = StyleSheet.create({
     topo:{
         width: "100%",
         height: 375 / 667 * width,
+    },
+    titulo:{
+        width: "100%",
+        position: "absolute",
+        textAlign: "center",
+        fontSize: 16,
+        lineHeight: 16,
+        color: "white",
+        fontWeight: "bold",
+        padding: 16
     }
 })
